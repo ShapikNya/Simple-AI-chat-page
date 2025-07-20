9ec5be65e34480d5e93c1daac636db83efadd672
ShapikNya <Shapik_psn@mail.ru>
2025-07-20 23:07:14 +0300

    Add Start Page

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  init()
+})
+function init() {
+  if(localStorage.getItem('theme')) {
+    document.documentElement.setAttribute("theme", "dark");
+  }
+  else {
+    document.documentElement.removeAttribute("theme");
+  }
+  
+}
+const toggleBtn = document.querySelector("#suka");
+toggleBtn.addEventListener("click", function() {
+  if(document.documentElement.hasAttribute("theme")){
+    document.documentElement.removeAttribute("theme");
+    localStorage.removeItem('theme');
+  }
+  else{
+    document.documentElement.setAttribute("theme", "dark");
+    localStorage.setItem('theme', 1);
+  }
+});
